1c437aa55d8a625115ff96dad48d988881e385ed
applevalley <skatkd7571@naver.com>
2021-06-25 23:58:52 +0900

    0625

--- BOJ/1987. 알파벳(풀이중)/boj_1987.py
+++ BOJ/1987. 알파벳(풀이중)/boj_1987.py
@@ -2,7 +2,11 @@
 # 인접한 4방향으로 이동하는데, 이동한 좌표에 있는 알파벳은 그 동안 지나온 알파벳과는 달라야 한다.
 # 좌상단까지 포함했을 때, 최대 몇 칸까지 갈 수 있는가?
 
-# 시간초과ㅠ
+# 시간초과가 발생한 코드
+# 처음에는 리스트로 저장했었는데, 각 방향이 리스트에 담겨있는지를 확인하는 과정에서 
+# 매 시도마다 리스트의 요소 개수만큼의 연산이 이루어지기에 시간초과가 난다 생각하여 집합으로 변경하였다.
+# 그럼에도 시간초과는 발생하였다. 리스트에서 집합으로 바꾼 만큼 약간의 시간 절약은 있었을 수 있다.
+# 하지만 여전히 시간초과가 나는 점에서 문제가 그것이 아니라는 생각이 들었다.
 
 import sys
 sys.setrecursionlimit(100000)
@@ -37,32 +41,31 @@ print(cnt)
 
 
 
+# 통과한 코드
+# DFS가 아닌 BFS로 구성하였다.
+# 아래와 같이 집합에 좌표와 해당 좌표의 알파벳을 같이 저장하면 (3, 0, 'A')와 같이 저장된다.
+# 글자는 좌표들을 지날수록 합쳐져 (1, 0, 'ACBDE')와 같이 늘어난다.
+# 아래와 같이 저장하면, 이전의 시도들과 같이 요소 확인을 위해 순회를 여러 번 할 필요가 없게 된다.
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
+def BFS():
+    cnt = 1       # !!!!! 0으로 했더니 1 1 A 와 같은 데이터에서 1이 아닌 0이 나와버렸다
 
+    while alpha:
+        x, y, check = alpha.pop()
+        for i in range(4):
+            tx, ty = x + dx[i], y + dy[i]
+            if 0 <= tx < R and 0 <= ty < C and arr[tx][ty] not in check:
+                check_plus = check + arr[tx][ty]
+                alpha.add((tx, ty, check_plus))
+                cnt = max(cnt, len(check_plus))
 
+    return cnt
 
+R, C = map(int, input().split())
+arr = [input() for _ in range(R)]
+alpha = set([(0, 0, arr[0][0])])
 
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
-#
-# from collections import deque
-#
-# def BFS(x, y):
-#     Q = deque()
-#     Q.append([x, y])
-#
-#     while Q:
-#         x, y = Q.popleft()
-#         for i in range(4):
-#             tx, ty = x + dx[i], y + dy[i]
-#             if 0 <= tx < R and 0 <= ty < C and arr[tx][ty] not in alpha:
-#                 Q.append([tx, ty])
-#                 alpha.append(arr[tx][ty])
-#
-# R, C = map(int, input().split())
-# arr = [input() for _ in range(R)]
-# alpha = [arr[0][0]]
-#
-# BFS(0, 0)
-# print(alpha)
+print(BFS())
