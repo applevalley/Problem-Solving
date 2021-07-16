eae845aba7ffa9b17a44bf79c59b42974be5d6cf
applevalley <skatkd7571@naver.com>
2021-07-16 23:59:22 +0900

    0716

--- BOJ/16918. 봄버맨(풀이중)/boj_16918.py
+++ BOJ/16918. 봄버맨(풀이중)/boj_16918.py
@@ -13,35 +13,38 @@
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bomb(x, y, value):
+def bomb():
     global cnt
+    Q = deque()
 
+    # 초기값, 또는 1초인 경우
     if cnt <= 1:
         for i in range(R):
             for j in range(C):
-                if arr[i][j] == 'O' and timer[i][j] == 0:
-                    timer[i][j] = 1
-                elif arr[i][j] == 'O' and timer[i][j]:
+                if arr[i][j] == 'O':
                     timer[i][j] += 1
     else:
         for i in range(R):
             for j in range(C):
-                if arr[i][j] == 'O' and timer[i][j] == 3:
-                    arr[tx][ty] = '.'
-                    timer[tx][ty] = 0
+                if arr[i][j] == 'O' and timer[i][j] == 3:           # 3초가 되어 폭발할 좌표인 경우 모아서 따로 처리해준다.
+                    Q.append([i, j])                                # 여기서 바로 처리하면 폭발하지 않는 좌표가 생겨버리기 때문
 
-                    for k in range(4):
-                        tx, ty = i + dx[k], j + dy[k]
-                        if 0 <= tx < R and 0 <= ty < C and arr[tx][ty] == 'O':
-                            arr[tx][ty] = '.'
-                            timer[tx][ty] = 0
-
-                elif arr[i][j] == 'O' and timer[i][j] < 3:
+                elif arr[i][j] == 'O' and timer[i][j] < 3:          # 폭탄이 있는 좌표이면서 3초가 경과하지 않은 경우
                     timer[i][j] += 1
 
-                if cnt % 2 == 0 and arr[i][j] == '.':
+                if cnt % 2 == 0 and arr[i][j] == '.':               # 폭탄 심기
                     arr[i][j] = 'O'
                     timer[i][j] = 1
+        while Q:
+            x, y = Q.popleft()
+            arr[x][y] = '.'
+            timer[x][y] = 0
+
+            for k in range(4):
+                tx, ty = x + dx[k], y + dy[k]
+                if 0 <= tx < R and 0 <= ty < C and arr[tx][ty] == 'O':
+                    arr[tx][ty] = '.'
+                    timer[tx][ty] = 0
 
 
 
@@ -51,16 +54,13 @@ from collections import deque
 
 R, C, N = map(int, sys.stdin.readline().split())
 arr = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
-timer = [[0] * C for _ in range(R)]
+timer = [[0] * C for _ in range(R)]    # 폭탄 시간 계산을 위한 리스트 / 폭탄이 설치된 후 숫자를 올리기 시작해 3초가 되면 폭발한다
 cnt = 0
-print(*arr)
 
-while cnt < N:
-    for i in range(R):
-        for j in range(C):
-            bomb(i, j, timer[i][j])
-
-    for xx in arr:
-        print(*xx)
-
+# 왜 N + 1을 해야 제대로 동작하는지 이해가 되지 않는다. 어딘가 잘못 짠 곳이 있다는건데...
+while cnt < N + 1:
+    bomb()
     cnt += 1
+
+for x in arr:
+    print(*x)
